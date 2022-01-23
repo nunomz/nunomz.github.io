d0593c4da2473faf7883e71405cece02be4bd85f
Nuno <onunomoreira@gmail.com>
2022-01-23 21:48:25 +0000

    skills filter working

--- assets/js/main.js
+++ assets/js/main.js
@@ -261,4 +261,34 @@
     })
   });
 
+  /**
+   * skills
+   */
+   window.addEventListener('load', () => {
+    let skillsContainer = select('.skills-container');
+    if (skillsContainer) {
+      let skillsIsotope = new Isotope(skillsContainer, {
+        itemSelector: '.skills-item'
+      });
+
+      let skillsFilters = select('#skills-flters li', true);
+
+      on('click', '#skills-flters li', function(e) {
+        e.preventDefault();
+        skillsFilters.forEach(function(el) {
+          el.classList.remove('filter-active');
+        });
+        this.classList.add('filter-active');
+
+        skillsIsotope.arrange({
+          filter: this.getAttribute('data-filter')
+        });
+        skillsIsotope.on('arrangeComplete', function() {
+          AOS.refresh()
+        });
+      }, true);
+    }
+
+  });
+
 })()
